b9b1ef58213b73e0534126da004fe811765f9909
Tim Plump <tiplump@gmail.com>
2015-05-26 14:34:15 -0400

    useless commit

--- src/com/tp/physics/ForceHandler.java
+++
@@ -1,5 +0,0 @@
-package com.tp.physics;
-
-public class ForceHandler {
-
-}
---
+++ src/com/tp/physics/ParticleForceGenerator.java
@@ -0,0 +1,9 @@
+package com.tp.physics;
+
+/**
+ * Created by timplump on 5/25/15.
+ */
+public interface ParticleForceGenerator {
+
+
+}
